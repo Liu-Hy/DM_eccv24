3c8f3420c018bb4f881c7095ee124f7409519a8e
NayameoW <naya1207445468@foxmail.com>
2024-02-24 14:33:11 +0800

    fix minor

--- networks.py
+++ networks.py
@@ -514,6 +514,16 @@ class ResNetImageNet(nn.Module):
         out = self.classifier(out)
         return out
 
+    def embed(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        # out = F.avg_pool2d(out, 4)
+        # out = out.view(out.size(0), -1)
+        return out
+
 def ResNet18BN(channel, num_classes):
     return ResNet(BasicBlock, [2,2,2,2], channel=channel, num_classes=num_classes, norm='batchnorm')
 
